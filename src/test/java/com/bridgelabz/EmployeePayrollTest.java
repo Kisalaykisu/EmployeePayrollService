d0e3165556b2ad103261502753389da845a382f0
Kisalay <kisalaykisu@gmail.com>
2022-07-26 19:43:18 +0530

    UC2 has been added:

--- src/test/java/com/bridgelabz/EmployeePayrollTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollTest.java
@@ -17,17 +17,17 @@ public class EmployeePayrollTest {
         };
         EmployeePayrollService employeePayrollService;
         employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
-        employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+        employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
 
-        employeePayrollService.printData(IOService.FILE_IO);
-        long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
         Assertions.assertEquals(3, entries);
 
     }
     @Test
     public void givenFile_WhenRead_ShouldReturnNumberOfEntries() {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-        long entries = employeePayrollService.readDataFromFile(IOService.FILE_IO);
+        long entries = employeePayrollService.readDataFromFile(EmployeePayrollService.IOService.FILE_IO);
         Assertions.assertEquals(3, entries);
     }
 }
